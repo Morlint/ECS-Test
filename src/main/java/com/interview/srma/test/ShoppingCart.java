e3b202b7395cdd9d77f5c557ccec79e1748582a9
Morlin T <Morlin T>
2016-04-29 11:49:43 +0100

    modified code to apply buy one get one for apples

--- src/main/java/com/interview/srma/test/ShoppingCart.java
+++ src/main/java/com/interview/srma/test/ShoppingCart.java
@@ -14,45 +14,43 @@ import java.util.List;
  */
 public class ShoppingCart {
 
-	private int totalNoOfItems =0;
-	private BigDecimal totalPrice=new BigDecimal("0");
+	private int totalNoOfItems = 0;
+	private BigDecimal totalPrice = new BigDecimal("0");
 	private List<Item> itemList = new ArrayList<Item>();
-	
-	//buy one get one
+
+	// buy one get one
 	private boolean buyOneGetOne;
-	int totalNoOfItemsForBuyOneGetOne=0;
-	
+	int totalNoOfItemsForBuyOneGetOne = 0;
+
+	// buy two get one
+	private boolean buyTwoGetOne;
+	int totalNoOfItemsForBuyTwoGetOne = 0;
+
 	public void addItem(Item item, int i) {
 		// TODO Auto-generated method stub
-		
+
 		totalNoOfItems += i;
-		
-		totalPrice = totalPrice.add(item.getPrice().multiply(new BigDecimal(i)));		
-		
-		if(totalNoOfItems > 1 && buyOneGetOne)
-		{
-			if((totalNoOfItems%2) == 1)
-			{
-				++totalNoOfItems;				
+
+		totalPrice = totalPrice.add(item.getPrice().multiply(new BigDecimal(i)));
+
+		if (totalNoOfItems > 1 && buyOneGetOne) {
+			if ((totalNoOfItems % 2) == 1) {
+				++totalNoOfItems;
 			}
 			totalPrice = totalPrice.add(item.getPrice().multiply(new BigDecimal(i)));
-			totalPrice = totalPrice.divide(new BigDecimal("2"));			
-		}	
-		else
-		{
+			totalPrice = totalPrice.divide(new BigDecimal("2"));
+		} else {
 			totalPrice = totalPrice.add(item.getPrice().multiply(new BigDecimal(i)));
 		}
-		
-		
+
 	}
-	
+
 	public int getTotalNoOfItems() {
 		// TODO Auto-generated method stub
 		return totalNoOfItems;
 	}
 
-	public void setTotalNoOfItems(int itemsBought)
-	{
+	public void setTotalNoOfItems(int itemsBought) {
 		this.totalNoOfItems = itemsBought;
 	}
 
@@ -60,17 +58,17 @@ public class ShoppingCart {
 		// TODO Auto-generated method stub
 		return totalPrice;
 	}
-	
+
 	public void setTotalPrice(BigDecimal price) {
 		// TODO Auto-generated method stub
-		totalPrice= price;
+		totalPrice = price;
 	}
 
 	public void setBuyOneGetOne(boolean buyOneGetOne) {
 		// TODO Auto-generated method stub
 		this.buyOneGetOne = buyOneGetOne;
 	}
-	
+
 	public boolean isBuyOneGetOne() {
 		return buyOneGetOne;
 	}
@@ -83,32 +81,30 @@ public class ShoppingCart {
 		this.itemList = itemList;
 	}
 
-	
-	//now all are apples
+	// now all are apples
 	public void checkout() {
 		// TODO Auto-generated method stub
-		if(buyOneGetOne)
-		{
-			if((totalNoOfItems%2) == 1)
-			{
-				++totalNoOfItems;				
+		if (buyOneGetOne) {
+			if ((totalNoOfItems % 2) == 1) {
+				++totalNoOfItems;
 			}
 			totalPrice = totalPrice.divide(new BigDecimal("2"));
 		}
-		
+
 	}
-	
-	public void applyBuyOneGetOneOffer()
-	{
-		if(buyOneGetOne && totalNoOfItemsForBuyOneGetOne >0)
-		{
-			
+
+	public void applyBuyOneGetOneOffer() {
+		if (buyOneGetOne && totalNoOfItemsForBuyOneGetOne > 0) {
+
 		}
 	}
 
+	public boolean isBuyTwoGetOne() {
+		return buyTwoGetOne;
+	}
 
+	public void setBuyTwoGetOne(boolean buyTwoGetOne) {
+		this.buyTwoGetOne = buyTwoGetOne;
+	}
 
-	
-	
-	
 }
